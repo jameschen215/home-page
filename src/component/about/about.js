f36c6dabbdb10385bd69a168f4a3ae362352067a
James <26018087+Jameschan215@user.noreply.github.com>
2025-04-02 16:18:43 +0800

    Fine tune mobile and tablet layout

--- src/component/about/about.js
+++ src/component/about/about.js
@@ -2,6 +2,7 @@ import './about.css';
 
 import photoMobile from '../../images/banner-photo@mobile.jpg';
 import photoTablet from '../../images/banner-photo@tablet.jpg';
+import photoDesktop from '../../images/banner-photo@desktop.jpg';
 
 import {
 	GITHUB_ICON,
@@ -11,25 +12,24 @@ import {
 
 export function About() {
 	const html = `
-    <section id='about' class='about'>  
-    
-      <div class='banner'>
-        <div class='bg-shadow'>
-          <div class='bg'></div>
-        </div>
+    <section id='about' class='about'> 
+      <div class='bg-shadow'>
+        <div class='bg'></div>
+      </div> 
 
+      <div class='banner'>
         <div class='photo'>
           <picture>
             <source media='(min-width: 481px)' srcset='${photoTablet}'>
+            <source media='(min-width: 961px)' srcset='${photoDesktop}'>
             <img src='${photoMobile}' alt='James photo' />
           </picture>
 
-          <div class='name'>
-            <h1>James</h1>
-            <h1>Chen</h1>
-          </div>
+          <h1 class='name'>
+            <span>James</span>
+            <span>Chen</span>
+          </h1>
         </div>
-
       </div>
     
       <div class='description'>
@@ -42,16 +42,21 @@ export function About() {
           nisi Dus aute irure dolor In reprehenderit in volupta
           te velit esse Cillum dolore eu tugiat nulla pariatur.
         </p>
-      </div>
 
-      <div class="social-links">
-        <a href="#"><div class='icon'>${GITHUB_ICON}</div></a>
-        <a href="#"><div class='icon'>${LINKEDIN_ICON}</div></a>
-        <a href="#"><div class='icon'>${TWITTER_ICON}</div></a>
-
+        <div class="social-links">
+          <a href="#"><div class='icon'>${GITHUB_ICON}</div></a>
+          <a href="#"><div class='icon'>${LINKEDIN_ICON}</div></a>
+          <a href="#"><div class='icon'>${TWITTER_ICON}</div></a>
+        </div>
       </div>
     </section>
   `;
 
 	return html;
+}
+
+{
+	/* <div class="bg-shadow">
+	<div class="bg"></div>
+</div>; */
 }
